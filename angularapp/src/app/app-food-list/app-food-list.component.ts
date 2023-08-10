38c299422d8349db680ada885c97cef3263dde2a
Iamneo <techadmin@iamneo.ai>
2023-08-10 12:39:01 +0000

    Backup Commit

--- angularapp/src/app/app-food-list/app-food-list.component.ts
+++ angularapp/src/app/app-food-list/app-food-list.component.ts
@@ -1,33 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Food {
+  name: string;
+  calories: number;
+  image: string;
+  quantity?: number; // Optional property for quantity
+}
+
 @Component({
   selector: 'app-food-list',
   templateUrl: './app-food-list.component.html',
   styleUrls: ['./app-food-list.component.css']
 })
 export class AppFoodListComponent {
-  foods = [
-    { name: 'Banana', calories: 100, imageSrc: 'path/to/banana.jpg' },
-    { name: 'Salmon', calories: 250, imageSrc: 'path/to/salmon.jpg' },
-    { name: 'Cake', calories: 400, imageSrc: 'path/to/cake.jpg' }
-  ];
+  foods: Food[] = []; // Initialize with your food data
+  newFood: Food = { name: '', calories: 0, image: '' };
+  todaysFoods: Food[] = [];
+  searchText: string = '';
+  showAddForm = false;
 
-  searchText = '';
-  filteredFoods = [...this.foods];
-  todaysFoods: any[] = [];
-  totalCalories = 0;
-
-  addToTodaysFoods(food: any) {
-    const existingFood = this.todaysFoods.find(item => item.name === food.name);
-    if (existingFood) {
-      existingFood.quantity++;
-    } else {
-      this.todaysFoods.push({ ...food, quantity: 1 });
-    }
-    this.calculateTotalCalories();
+  addFood() {
+    this.foods.push(this.newFood);
+    this.newFood = { name: '', calories: 0, image: '' };
+    this.showAddForm = false;
   }
 
-  calculateTotalCalories() {
-    this.totalCalories = this.todaysFoods.reduce((total, food) => total + food.calories * food.quantity, 0);
+  addToTodaysFoods(food: Food, quantity: number = 1) {
+    for (let i = 0; i < quantity; i++) {
+      this.todaysFoods.push(food);
+    }
   }
 }
--- angularapp/src/app/food-filter.pipe.ts
+++ angularapp/src/app/food-filter.pipe.ts
@@ -5,9 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FoodFilterPipe implements PipeTransform {
   transform(foods: any[], searchText: string): any[] {
-    if (!searchText) {
+    if (!foods || !searchText) {
       return foods;
     }
+
     searchText = searchText.toLowerCase();
     return foods.filter(food => food.name.toLowerCase().includes(searchText));
   }
